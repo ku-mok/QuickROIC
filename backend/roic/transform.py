90e61b32f32680f3bf8b2b73e20b959a8331dc54
kueda <kei.mokusyun@gmail.com>
2021-09-20 08:59:13 +0000

    ２つ以上のエクセルファイルを読み取って変換する部分のテストが不足していたので追加

--- backend/roic/transform.py
+++ backend/roic/transform.py
@@ -16,7 +16,7 @@ def speeda_excel_to_dataframe(book_path_list: list[str]) -> pd.DataFrame:
         pd.DataFrame: SPEEDAからダウンロードしたファイルを成形したDataFrame
     """
     sheet_data_list: list[pd.DataFrame] = []
-    company_master = pd.DataFrame()
+    company_master_list: list[pd.DataFrame] = []
     for book in book_path_list:
         sheet_data = pd.read_excel(book, skiprows=7, header=[0, 1])
         # カラム名(エクセルの8,9行)は科目名と年度になっており、科目名は空白の場合があるのでUnnamedを空白に置き換えて列名とする
@@ -39,13 +39,17 @@ def speeda_excel_to_dataframe(book_path_list: list[str]) -> pd.DataFrame:
             .assign(年度=lambda d: d.年度.str.replace("年度通期", ""))
         # 成形したデータを後からマージできるようにリストに追加する
         sheet_data_list.append(sheet_data)
+        company_master_list.append(company_master)
     # 各シートから成形したデータをマージする
     if len(sheet_data_list) == 1:
         company_data = sheet_data_list[0]
+        company_master = company_master_list[0]
     else:
         company_data = sheet_data_list[0]
+        company_master = company_master_list[0]
         for i in range(1, len(sheet_data_list)):
             company_data = company_data.merge(sheet_data_list[i], on=["企業名称", "年度"])
+            company_master = company_master.merge(company_master_list[i], on=["企業名称"])
 
     # βはLTMしか存在しないためマスタ系に移す
     beta_col = company_data.filter(like="β").columns
@@ -77,6 +81,7 @@ def dataframe_to_dict(df: pd.DataFrame, columns: list[str] | Literal["all"] = "a
         .stack(0)\
         .reset_index(1)\
         .drop(columns="level_1")\
+        .applymap(lambda x: int(x) if int(x) == x else x)\
         .to_dict("index")
     # GraphQLで扱いやすい辞書に変換
     return [
