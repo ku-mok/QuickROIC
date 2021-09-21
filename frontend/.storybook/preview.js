4bfab966d237b9649b2988990009f56bf76f74c6
kueda <kei.mokusyun@gmail.com>
2021-09-21 05:22:42 +0000

    Muiバージョンへのstorybookの対応

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,10 @@
+import React from "react";
+import { ThemeProvider as SsThemeProvider } from "styled-components";
+import {
+  ThemeProvider as MuiThemeProvider,
+  StyledEngineProvider,
+} from "@mui/material/styles";
+import theme from "../src/theme";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +13,16 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <StyledEngineProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <SsThemeProvider theme={theme}>
+          <Story />
+        </SsThemeProvider>
+      </MuiThemeProvider>
+    </StyledEngineProvider>
+  ),
+];
