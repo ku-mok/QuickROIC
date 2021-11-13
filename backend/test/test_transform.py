550db0371107f28a44c2a04739fa84ba9f7cdf19
kueda <kei.mokusyun@gmail.com>
2021-11-13 22:10:20 +0900

    fix #25

--- backend/test/test_transform.py
+++ backend/test/test_transform.py
@@ -2,6 +2,8 @@ import unittest
 from roic.transform import speeda_excel_to_dataframe
 from pandas.testing import assert_frame_equal
 from roic import dataframe_to_dict, dict_to_dataframe
+import numpy as np
+from parameterized import parameterized
 import pandas as pd
 import os
 
@@ -180,36 +182,51 @@ class TestSpeedaTransform(unittest.TestCase):
 
 
 class TestTranformDfDict(unittest.TestCase):
-    def test_unaltered_after_df_to_dict_df(self):
+    @parameterized.expand([
+        ({'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+          '年度': [2014, 2015, 2016]*3,
+          '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
+          '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]},),
+        # floatとintが混ざっっている時
+        ({'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+          '年度': [2014, 2015, 2016]*3,
+          '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
+          '指標2': [2.0, 4.0, 5.4, 7.5, 9.1, 12, 15, 15, 17]},),
+        # strが混ざっている時
+        ({'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+          '年度': [2014, 2015, 2016]*3,
+          '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
+          '指標2': [2.0, 4.0, 5.4, 7.5, 9.1, 12, 15, 15, 17],
+          '指標3': ["SomeText"] * 9},),
+        # 一部年度にnullが混ざっている時
+        ({'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+          '年度': [2014, 2015, 2016]*3,
+          '指標1': [None, None, 5, 7, 9, 11, 13, 15, 17],
+          '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]},),
+        # ある指標すべてがnullの時
+        ({'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+          '年度': [2014, 2015, 2016]*3,
+          '指標1': [None, None, None, 7, 9, 11, 13, 15, 17],
+          '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]},),
+    ])
+    def test_unaltered_after_df_to_dict_df(self, input_df_dict):
         # データフレームから辞書・辞書からデータフレームが逆変換になっているかを確認
-        input = pd.DataFrame.from_dict(
-            {'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
-             '年度': [2014, 2015, 2016]*3,
-             '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
-             '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]
-             })
+        input = pd.DataFrame.from_dict(input_df_dict)
         assert_frame_equal(input, dict_to_dataframe(dataframe_to_dict(input)), check_like=True)
 
-    def test_unaltered_after_df_to_dict_df_int_float(self):
-        # floatとintが混在している場合の変換が逆変換になっているか
-        input = pd.DataFrame.from_dict(
-            {'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
-             '年度': [2014, 2015, 2016]*3,
-             '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
-             '指標2': [2.0, 4.0, 5.4, 7.5, 9.1, 12, 15, 15, 17]
-             })
-        assert_frame_equal(input, dict_to_dataframe(dataframe_to_dict(input)), check_like=True)
-
-    def test_unaltered_after_df_to_dict_df_int_str(self):
-        # strとnumericが混在している場合の変換が逆変換になっているか
-        input = pd.DataFrame.from_dict(
-            {'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
-             '年度': [2014, 2015, 2016]*3,
-             '指標1': [1, 3, 5, 7, 9, 11, 13, 15, 17],
-             '指標2': [2.0, 4.0, 5.4, 7.5, 9.1, 12, 15, 15, 17],
-             '指標3': ["SomeText"] * 9
-             })
-        assert_frame_equal(input, dict_to_dataframe(dataframe_to_dict(input)), check_like=True)
+    def test_unaltered_after_df_to_dict_df_inf(self):
+        # データフレームから辞書・辞書からデータフレームが逆変換になっているかを確認(inf対応)
+        input_df_dict = {'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+                         '年度': [2014, 2015, 2016]*3,
+                         '指標1': [np.inf, np.inf, np.inf, 7, 9, 11, 13, 15, 17],
+                         '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]}
+        expect_df_dict = {'企業名称': ['Sample1']*3 + ['Sample2']*3 + ['Sample3']*3,
+                          '年度': [2014, 2015, 2016]*3,
+                          '指標1': [0, 0, 0, 7, 9, 11, 13, 15, 17],
+                          '指標2': [2, 4, 5, 7, 9, 12, 15, 15, 17]}
+        input = pd.DataFrame.from_dict(input_df_dict)
+        expect = pd.DataFrame.from_dict(expect_df_dict)
+        assert_frame_equal(expect, dict_to_dataframe(dataframe_to_dict(input)), check_like=True)
 
     def test_select_columns(self):
         input = pd.DataFrame.from_dict(
